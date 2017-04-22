0d53f6b8fc05bcf7b8f93a6e4f8ac3a4e2a25eff
scarlettmichanco <scarlett@vynyl.com>
2017-04-22 14:21:40 -0700

    add health bar scripts

---
+++ Assets/Scripts/HealthBarController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBarController : MonoBehaviour {
+
+    // Use this for initialization
+    private Slider healthBar;
+    private int currentHealthPoints = 100;
+
+	void Awake () {
+        healthBar = GetComponent<Slider>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        healthBar.value = currentHealthPoints;
+	}
+
+    public void changeHealthPoints(int dHP) {
+        currentHealthPoints += dHP;
+    }
+}
---
+++ Assets/Scripts/TestHealthController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestHealthController : MonoBehaviour {
+
+    private HealthBarController healthBar;
+
+    private void Awake () {
+        healthBar = GameObject.Find("healthbar").GetComponent<HealthBarController>();
+    }
+    
+	
+	// Update is called once per frame
+	void Update () {
+	    if (Input.GetKey(KeyCode.UpArrow)) healthBar.changeHealthPoints(1);
+        if (Input.GetKey(KeyCode.DownArrow)) healthBar.changeHealthPoints(-1);
+	}
+}
