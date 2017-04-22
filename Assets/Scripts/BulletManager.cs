24eb7026b839ffbd6e9229f13f6e6b316aabe3a8
Robert <bobby@vynyl.com>
2017-04-21 23:06:07 -0600

    added armor, bullet and health managers

---
+++ Assets/Scripts/BulletManager.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Assets.Scripts
+{
+    public enum DamageType
+    {
+        Physical = 0x0,
+        Fire= 0x1,
+        Ice= 0x2,
+        Poison=0x4
+    }
+
+    public class BulletManager : MonoBehaviour
+    {
+        /// <summary>
+        /// inital damage inflicted
+        /// </summary>
+        public int damage = 1;
+
+        /// <summary>
+        /// Damage over time
+        /// </summary>
+        public bool hasDamageOverTime = false;
+
+        /// <summary>
+        /// number of seconds the dot is inflicted over
+        /// </summary>
+        public int damageTickSeconds = 8;
+
+        /// <summary>
+        /// Damage over time
+        /// </summary>
+        public int damageOverTime = 0;
+
+        /// <summary>
+        /// Type of damage sustained
+        /// </summary>
+        public DamageType damageType = DamageType.Physical;
+
+        /// <summary>
+        /// is this a shot from the enemy
+        /// </summary>
+        public bool isFromEnemy = false;
+
+        // Use this for initialization
+        void Start()
+        {
+            Destroy(gameObject, 20);
+        }
+    }
+}
---
+++ Assets/Scripts/HealthManager.cs
@@ -0,0 +1,78 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+namespace Assets.Scripts
+{
+
+    public enum MobileType
+    {
+        Player,
+        Enemy,
+        NPC
+    }
+    public class HealthManager : MonoBehaviour
+    {
+
+        /// <summary>
+        /// Total max hitpoints
+        /// </summary>
+        public int MaxHp = 1;
+
+        /// <summary>
+        /// Defines whether the object is enemy, player or npc
+        /// </summary>
+        public MobileType EntityType = MobileType.Enemy;
+
+        public void Damage(int damageAmount, DamageType type)
+        {
+            var armor = gameObject.GetComponent<ArmorManager>();
+            if (armor != null)
+            {
+                damageAmount = armor.CalculateDamage(damageAmount, type);
+            }
+            if (currentHp - damageAmount > 0)
+            {
+                //dead
+                Destroy(gameObject);
+            }
+            else
+            {
+                currentHp -= damageAmount;
+            }
+        }
+
+        public void Heal(int healAmount)
+        {
+            if (currentHp + healAmount > MaxHp)
+            {
+                currentHp = MaxHp;
+            }
+            else
+            {
+                currentHp += healAmount;
+            }
+        }
+
+        // Use this for initialization
+        void Start()
+        {
+            currentHp = MaxHp;
+        }
+
+        void OnTriggerEnter2D(Collider2D other)
+        {
+            var bulletScript = other.gameObject.GetComponent<BulletManager>();
+            if (bulletScript != null)
+            {
+                if ((bulletScript.isFromEnemy && (EntityType == MobileType.Player || EntityType == MobileType.NPC)) || (!bulletScript.isFromEnemy && EntityType == MobileType.Enemy))
+                {
+                    Damage(bulletScript.damage, bulletScript.damageType);
+                }
+
+            }
+        }
+
+        private int currentHp;
+    }
+}
