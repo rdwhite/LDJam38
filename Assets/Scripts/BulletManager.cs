b8ffc772a52687cfebd545cfdb543e5f4f236b28
Robert <bobby@vynyl.com>
2017-04-22 11:46:16 -0600

    addition of bullet pattern library

--- Assets/Scripts/BulletManager.cs
+++ Assets/Scripts/BulletManager.cs
@@ -1,52 +1,53 @@
-﻿using UnityEngine;
-using System.Collections;
+﻿// This script should be attached to ONE gameObject that is always present
+// Its necessary for handling orphan bullets who have no BulletPattern father and is required for all order and harmony in
+// the Bullet Universe, hence the name - BulletManager
 
-namespace Assets.Scripts
+using UnityEngine;
+using System.Collections.Generic;
+
+public class BulletManager : MonoBehaviour
 {
-    public enum DamageType
+
+    [SerializeField]
+    public List<BulletList> bullets = new List<BulletList>();
+
+    public Bullet[] bulletPrefab;
+
+    public Transform player;
+    public Transform MainCam;
+
+    public int bulletCount = 0;
+    public float rank = 0f;
+    public float bulletLifespan = 5f;
+
+
+    //DO NOT change this value unless you want every time-related pattern action to be affected accordingly
+    //The value is multipled by  every user-inputted time variable so that the frame numbers will be converted to smaller
+    //second based times that the Unity functions (yield.WaitForSWeconds) will accept.
+    [HideInInspector]
+    public float timePerFrame = 0.01666f;
+    //original value = 0.01666; (approximately one 60th of a second)
+
+    public static BulletManager instance;
+
+
+    void Awake()
     {
-        Physical = 0x0,
-        Fire= 0x1,
-        Ice= 0x2,
-        Poison=0x4
-    }
+        instance = this;
 
-    public class BulletManager : MonoBehaviour
-    {
-        /// <summary>
-        /// inital damage inflicted
-        /// </summary>
-        public int damage = 1;
-
-        /// <summary>
-        /// Damage over time
-        /// </summary>
-        public bool hasDamageOverTime = false;
-
-        /// <summary>
-        /// number of seconds the dot is inflicted over
-        /// </summary>
-        public int damageTickSeconds = 8;
-
-        /// <summary>
-        /// Damage over time
-        /// </summary>
-        public int damageOverTime = 0;
-
-        /// <summary>
-        /// Type of damage sustained
-        /// </summary>
-        public DamageType damageType = DamageType.Physical;
-
-        /// <summary>
-        /// is this a shot from the enemy
-        /// </summary>
-        public bool isFromEnemy = false;
-
-        // Use this for initialization
-        void Start()
+        for (var i = 0; i < bulletPrefab.Length; i++)
         {
-            Destroy(gameObject, 20);
+            bullets.Add(new BulletList());
         }
     }
+
+
+}
+
+
+[System.Serializable]
+public class BulletList
+{
+    [SerializeField]
+    public List<Bullet> bl = new List<Bullet>();
 }
