fecc2d0d0aad085f68e43d8c71fa5cba533082d5
Robert <bobby@vynyl.com>
2017-04-23 13:07:05 -0600

    Rename player controller

--- Assets/Scripts/ObjectPickupController.cs
+++ Assets/Scripts/ObjectPickupController.cs
@@ -7,7 +7,7 @@ public class ObjectPickupController : MonoBehaviour {
     
     void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.gameObject.name == "Basic Player Controller")
+        if (other.gameObject.name == "PlayerController")
         {
             gameObject.SetActive(false);
         }
